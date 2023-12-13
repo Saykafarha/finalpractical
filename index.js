c6d257e5d5739c08fdaf738e96afd01595042be6
Saykafarha <146607806+Saykafarha@users.noreply.github.com>
2023-12-13 12:01:43 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ function searchWeather() {
         .then(response => response.json())
         .then(data => {
             const location = data.name + ", " + data.sys.country;
-            const temperature = Math.round(data.main.temp - 273.15) + "°C";
+            const temperature = Math.round(data.main.temp) + "°C";
             const description = data.weather[0].description;
+            const windSpeed = "Wind Speed: " + data.wind.speed + " m/s";
+            const humidity = "Humidity: " + data.main.humidity + "%";
             const iconCode = data.weather[0].icon;
             const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
 
             document.getElementById("location").innerText = location;
             document.getElementById("temperature").innerText = temperature;
             document.getElementById("description").innerText = description;
+            document.getElementById("wind").innerText = windSpeed;
+            document.getElementById("humidity").innerText = humidity;
             document.getElementById("weather-icon").src = iconUrl;
+
+            document.getElementById("weather-container").style.display = "block";
         })
-        .catch(error => console.error("Error fetching data:", error));
+        .catch(error => {
+            console.error("Error fetching data:", error);
+            alert("City not found. Please enter a valid city name.");
+        });
 }
